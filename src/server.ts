1b5ec19a2ae1418a9413dd0c5405b30cadbf89cc
giovannimedici <giovannigilsu@gmail.com>
2024-07-01 21:03:15 -0300

    first commit

---
+++ src/server.ts
@@ -0,0 +1,19 @@
+import fastify, { FastifyInstance } from 'fastify';
+import { userRoutes } from './routes/user.routes';
+import { contactsRoutes } from './routes/contact.routes';
+const app: FastifyInstance = fastify();
+
+app.register(userRoutes, {
+    prefix: '/users',
+});
+
+app.register(contactsRoutes, {
+    prefix: '/contacts',
+});
+
+app.listen(
+    {
+        port: 3100
+    },
+    () => console.log('Server is running on port 3100'),
+);
